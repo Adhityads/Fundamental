afa443142475ac0a18fab332de5611fa29eb40fa
Adhitya Dharmawan Santosa <adhityadharmawan@gmail.com>
2022-10-04 20:45:56 +0700

    Update

---
+++ src/com/juaracoding/dayseven/bidang/RuangBidang.java
@@ -0,0 +1,12 @@
+package com.juaracoding.dayseven.bidang;
+
+public class RuangBidang {
+
+    String gambar = "default";
+    String namagambar = "default";
+
+    public void draw(){
+        System.out.println(gambar);
+        System.out.println(namagambar);
+    }
+}
